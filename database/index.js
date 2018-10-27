404a7b0ed72a2741d572ec53fc1b9e04b6bf6629
andrewjohnso7 <andrewjohnso17@gmail.com>
2018-10-26 19:27:00 -0700

    Update index.js

--- database/index.js
+++ database/index.js
@@ -2,5 +2,4 @@ var mongoose = require('mongoose');
 const mongoUri = 'mongodb://127.0.0.1:27017/buyingp';
 const db = mongoose.connect(mongoUri, { useNewUrlParser: true });
 
-console.log('this is db', db);
 module.exports = db;
