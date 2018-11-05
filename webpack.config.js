cdee8acbc3f810a83e8dd16fc0ed2216f932e803
andrewjohnso7 <andrewjohnso7@yahoo.com>
2018-11-05 13:10:43 -0800

    changed positioning of components

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,13 @@ module.exports = {
     filename: 'bundle.js', // what it will name the bundle
   },
   module: {
-    loaders: [
-      {test: /\.woff$/, loader: 'url?limit=10240&mimetype=application/font-woff'},
-      {test: /\.ttf$/, loader: 'url?limit=10240&mimetype=application/x-font-ttf'},
-      {test: /\.eot$/, loader: 'url?limit=10240&mimetype=application/vnd.ms-fontobject'},
-      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
-    ], 
+    // This is causing an error that makes webpack fail. They were here in an attempt to make webpack configure correctly.
+    // loaders: [
+    //   {test: /\.woff$/, loader: 'url?limit=10240&mimetype=application/font-woff'},
+    //   {test: /\.ttf$/, loader: 'url?limit=10240&mimetype=application/x-font-ttf'},
+    //   {test: /\.eot$/, loader: 'url?limit=10240&mimetype=application/vnd.ms-fontobject'},
+    //   { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
+    // ], 
     rules: [
       { // everything that babel needs to run
         test: /\.jsx?$/, // .js OR .jsx files
